77932ded185abe68b4319cf85fe8c26fea28571e
kiru <kiru@hulk.(none)>
2013-11-10 20:17:28 -0800

    blondies can be now saved and loaded

--- blondie42.py
+++ blondie42.py
@@ -1,44 +1,56 @@
 #!/home/kiru/pyenv/neuro2/bin/python
 
-import random
 import pybrain
 
-from pybrain.structure import FeedForwardNetwork, LinearLayer, SigmoidLayer, FullConnection
+import random
+import datetime
+import re
 
+from pybrain.structure import FeedForwardNetwork, \
+     LinearLayer, SigmoidLayer, FullConnection
+
+from pybrain.tools.xml.networkwriter import NetworkWriter
+from pybrain.tools.xml.networkreader import NetworkReader
+     
 class BlondieBrain:
-    def __init__(self,insize=150):
-        self.insize = insize
-        self.nn = FeedForwardNetwork()
+    def __init__(self,insize=150,paramfile=None,datadir='blondiehome'):
+        if paramfile:
+            self.nn = NetworkReader.readFrom(paramfile)
+            self.name = paramfile.split('.')[0]
+        else:
+            self.insize = insize
+            self.nn = FeedForwardNetwork()
+            tmpname = "blondie-%s"%(datetime.datetime.now())
+            self.name = re.sub('[.: ]', '-', tmpname)
 
-        inLayer = LinearLayer(insize)
-        hiddenLayer1 = SigmoidLayer(insize)
-        hiddenLayer2 = SigmoidLayer(insize)
-        outLayer = LinearLayer(1)
+            inLayer = LinearLayer(insize)
+            hiddenLayer1 = SigmoidLayer(insize)
+            hiddenLayer2 = SigmoidLayer(insize)
+            outLayer = LinearLayer(1)
 
-        self.nn.addInputModule(inLayer)
-        self.nn.addModule(hiddenLayer1)
-        self.nn.addModule(hiddenLayer2)
-        self.nn.addOutputModule(outLayer)
+            self.nn.addInputModule(inLayer)
+            self.nn.addModule(hiddenLayer1)
+            self.nn.addModule(hiddenLayer2)
+            self.nn.addOutputModule(outLayer)
 
-        in_to_hidden1 = FullConnection(inLayer, hiddenLayer1)
-        hidden1_to_hidden2 = FullConnection(hiddenLayer1, hiddenLayer2)
-        hidden2_to_out = FullConnection(hiddenLayer2, outLayer)
+            in_to_hidden1 = FullConnection(inLayer, hiddenLayer1)
+            hidden1_to_hidden2 = FullConnection(hiddenLayer1, hiddenLayer2)
+            hidden2_to_out = FullConnection(hiddenLayer2, outLayer)
 
-        self.nn.addConnection(in_to_hidden1)
-        self.nn.addConnection(hidden1_to_hidden2)
-        self.nn.addConnection(hidden2_to_out)
+            self.nn.addConnection(in_to_hidden1)
+            self.nn.addConnection(hidden1_to_hidden2)
+            self.nn.addConnection(hidden2_to_out)
 
-        self.nn.sortModules()
+            self.nn.sortModules()
 
     def activate(self,inputdata):
         return self.nn.activate(inputdata)
 
-    def printit(self):
-        print self.nn
+    def save(self):
+        NetworkWriter.writeToFile(self.nn, "blondestore/%s.xml"%(self.name))
     
 inp = [random.randint(0,6) for _ in range(150)]
-print inp
 
 n = BlondieBrain()
 n.activate(inp)
-n.printit()
+n.save()
