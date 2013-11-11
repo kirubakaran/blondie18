039fa4f1637af996864b40b0d8dcda98e4b5c891
kiru <kiru@hulk.(none)>
2013-11-11 08:49:57 -0800

    fixed deep copy problem

--- blondie42.py
+++ blondie42.py
@@ -5,6 +5,8 @@ import datetime
 import re
 import os
 import itertools
+import heapq
+import copy
 
 from pybrain.structure import FeedForwardNetwork, \
      LinearLayer, SigmoidLayer, FullConnection
@@ -52,9 +54,17 @@ class BlondieBrain:
     def nextmove(self,inputdata):
         return int(self.nn.activate(inputdata))
 
-    def save(self):
-        f = os.path.join(self.datadir,self.name+".xml")
+    def save(self,suffix=''):
+        f = os.path.join(self.datadir,self.name+suffix+".xml")
         NetworkWriter.writeToFile(self.nn, f)
+
+    def mutate(self):
+        self.nn.mutate()
+
+    def copy(self):
+        x = copy.deepcopy(self)
+        x.nn = x.nn.copy()
+        return x
 
 def trsymb(piece,mysymbol):
     #Transform symbol
@@ -80,15 +90,83 @@ def game2input(game):
     diags = [trsum(r,mysymbol) for r in game.diags]
     l = itertools.chain.from_iterable([cells,cols,rows,diags])
     return list(l)
-        
+
+def rungame(b1,b2):
+    return (random.randint(0,5),random.randint(0,5))
+
 def main(game):
-    # popsize = 100
-    # pop = [BlondieBrain() for _ in range(100)]
-    # print pop
-    n = BlondieBrain()
-    ip = game2input(game)
-    print "move = ", n.nextmove(ip)
-    n.save()
+    gen = 0
+    popsize = 20
+    pop = [BlondieBrain() for _ in range(100)]
+
+    #while gen > 10: #disabled
+    while gen < 10:
+        print "Generation : %s"%(gen,)
+        r = [0]*popsize
+        for i in range(popsize):
+            for j in range(popsize):
+                if i == j: continue
+                ri,rj = rungame(pop[i],pop[j])
+                r[i] += ri
+                r[j] += rj
+        #write best to disk
+        win  = r.index(max(r))
+        print "winner of gen %03d is %02d"%(gen,win)
+        best = pop[win]
+        best.save('-bestof-gen%03d'%(gen,))
+        #keep best 10% for next gen
+        bestn = heapq.nlargest(int(popsize/10.0),r)
+        print "bestn",bestn
+        newpop = []
+        for x in bestn:
+            newpop.append(pop[r.index(x)].copy())
+        spotsleft = popsize - len(newpop)
+        for n in range(spotsleft):
+            t = best.copy()
+            t.mutate()
+            newpop.append(t)
+        pop = newpop
+        gen += 1
+        
+    # n = BlondieBrain()
+    # for i in range(10):
+    #     ip = game2input(game)
+    #     print "m1", i, "move = ", n.nextmove(ip)
+    # n.save('-m1')
+    # n.nn.mutate()
+    # print('')
+    # for i in range(10):
+    #     ip = game2input(game)
+    #     print "m2", i, "move = ", n.nextmove(ip)
+    # n.save('-m2')
+    
+    # n = BlondieBrain()
+    # ip = game2input(game)
+    # #x = [copy.deepcopy(n) for _ in range(2)]
+    # x = [n.copy() for _ in range(2)]
+    # #x = [n for _ in range(10)]
+    # for xx in x:
+    #     print id(xx)
+    #     print ">",xx.nextmove(ip)
+    #     xx.mutate()
+    #     print "<",xx.nextmove(ip)
+    # print
+    # for xx in x:
+    #     print id(xx)
+    #     print xx.nextmove(ip)
+    # print "m1 move = ", n.nextmove(ip)
+    # m = copy.deepcopy(n)
+    # m.nn.mutate()
+    # ip = game2input(game)
+    # print "m2 move = ", m.nextmove(ip)
+
+    # ip = game2input(game)
+    # n = [BlondieBrain() for _ in range(10)]
+    # for i in range(10):
+    #     for nnx in n:
+    #         print nnx.nextmove(ip),
+    #         nnx.mutate()
+    #     print
     
 if __name__ == '__main__':
     game = runner.Game()
