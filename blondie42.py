761145eff0c6f36b4252a18e313fc3c4202cce16
kiru <kiru@hulk.(none)>
2013-11-11 09:05:50 -0800

    moved functions that belong in the blondie class, into blondie class

--- blondie42.py
+++ blondie42.py
@@ -19,21 +19,26 @@ import runner
 DATADIR = "/media/tera/blondiehome"
      
 class BlondieBrain:
-    def __init__(self,insize=139,paramfile=None,datadir=DATADIR):
+    def __init__(self,insize=None,paramfile=None,datadir=DATADIR):
         self.datadir = datadir
+        if insize == None:
+            g = runner.Game()
+            ip = self._game2input(g)
+            self.insize = len(ip)
+        else:
+            self.insize = insize
         if paramfile:
             f = os.path.join(datadir,paramfile)
             self.nn = NetworkReader.readFrom(f)
             self.name = paramfile.split('.')[0]
         else:
-            self.insize = insize
             self.nn = FeedForwardNetwork()
             tmpname = "blondie-%s"%(datetime.datetime.now())
             self.name = re.sub('[.: ]', '-', tmpname)
 
-            inLayer = LinearLayer(insize)
-            hiddenLayer1 = SigmoidLayer(insize)
-            hiddenLayer2 = SigmoidLayer(insize)
+            inLayer = LinearLayer(self.insize)
+            hiddenLayer1 = SigmoidLayer(self.insize)
+            hiddenLayer2 = SigmoidLayer(self.insize)
             outLayer = LinearLayer(1)
 
             self.nn.addInputModule(inLayer)
@@ -51,7 +56,8 @@ class BlondieBrain:
 
             self.nn.sortModules()
 
-    def nextmove(self,inputdata):
+    def nextmove(self,game):
+        inputdata = self._game2input(game)
         return int(self.nn.activate(inputdata))
 
     def save(self,suffix=''):
@@ -66,41 +72,42 @@ class BlondieBrain:
         x.nn = x.nn.copy()
         return x
 
-def trsymb(piece,mysymbol):
-    #Transform symbol
-    if piece == None:
-        return 0
-    elif piece == mysymbol:
-        return 1
-    else:
-        return -1
-
-def trsum(l,mysymbol):
-    #Transform symbol and sum list
-    s = 0
-    for ll in l:
-        s += trsymb(ll,mysymbol)
-    return s
-
-def game2input(game):
-    mysymbol = len(game.moves)%2
-    cells = [trsymb(x,mysymbol) for x in itertools.chain.from_iterable(game.grid_columns)]
-    cols  = [trsum(c,mysymbol) for c in game.grid_columns]
-    rows  = [trsum(r,mysymbol) for r in game.grid_rows]
-    diags = [trsum(r,mysymbol) for r in game.diags]
-    l = itertools.chain.from_iterable([cells,cols,rows,diags])
-    return list(l)
+    def _game2input(self,game):
+        mysymbol = len(game.moves)%2
+        cells = [self._trsymb(x,mysymbol) for x in itertools.chain.from_iterable(game.grid_columns)]
+        cols  = [self._trsum(c,mysymbol)  for c in game.grid_columns]
+        rows  = [self._trsum(r,mysymbol)  for r in game.grid_rows]
+        diags = [self._trsum(r,mysymbol)  for r in game.diags]
+        l = itertools.chain.from_iterable([cells,cols,rows,diags])
+        return list(l)
+        
+    def _trsymb(piece,mysymbol):
+        #Transform symbol
+        if piece == None:
+            return 0
+        elif piece == mysymbol:
+            return 1
+        else:
+            return -1
+     
+    def _trsum(l,mysymbol):
+        #Transform symbol and sum list
+        s = 0
+        for ll in l:
+            s += trsymb(ll,mysymbol)
+        return s
 
 def rungame(b1,b2):
     return (random.randint(0,5),random.randint(0,5))
 
 def main(game):
-    gen = 0
-    popsize = 20
-    pop = [BlondieBrain() for _ in range(100)]
+    popsize   = 20
+    keepratio = 10.0/100.0 #ratio of top performers to keep
+    maxgen    = 10
 
-    #while gen > 10: #disabled
-    while gen < 10:
+    gen = 0
+    pop = [BlondieBrain() for _ in range(popsize)]
+    while gen < maxgen:
         print "Generation : %s"%(gen,)
         r = [0]*popsize
         for i in range(popsize):
@@ -109,14 +116,15 @@ def main(game):
                 ri,rj = rungame(pop[i],pop[j])
                 r[i] += ri
                 r[j] += rj
+                
         #write best to disk
         win  = r.index(max(r))
         print "winner of gen %03d is %02d"%(gen,win)
         best = pop[win]
         best.save('-bestof-gen%03d'%(gen,))
-        #keep best 10% for next gen
-        bestn = heapq.nlargest(int(popsize/10.0),r)
-        print "bestn",bestn
+        
+        #keep top best for next gen
+        bestn = heapq.nlargest(int(popsize*keepratio),r)
         newpop = []
         for x in bestn:
             newpop.append(pop[r.index(x)].copy())
@@ -127,53 +135,6 @@ def main(game):
             newpop.append(t)
         pop = newpop
         gen += 1
-        
-    # n = BlondieBrain()
-    # for i in range(10):
-    #     ip = game2input(game)
-    #     print "m1", i, "move = ", n.nextmove(ip)
-    # n.save('-m1')
-    # n.nn.mutate()
-    # print('')
-    # for i in range(10):
-    #     ip = game2input(game)
-    #     print "m2", i, "move = ", n.nextmove(ip)
-    # n.save('-m2')
-    
-    # n = BlondieBrain()
-    # ip = game2input(game)
-    # #x = [copy.deepcopy(n) for _ in range(2)]
-    # x = [n.copy() for _ in range(2)]
-    # #x = [n for _ in range(10)]
-    # for xx in x:
-    #     print id(xx)
-    #     print ">",xx.nextmove(ip)
-    #     xx.mutate()
-    #     print "<",xx.nextmove(ip)
-    # print
-    # for xx in x:
-    #     print id(xx)
-    #     print xx.nextmove(ip)
-    # print "m1 move = ", n.nextmove(ip)
-    # m = copy.deepcopy(n)
-    # m.nn.mutate()
-    # ip = game2input(game)
-    # print "m2 move = ", m.nextmove(ip)
-
-    # ip = game2input(game)
-    # n = [BlondieBrain() for _ in range(10)]
-    # for i in range(10):
-    #     for nnx in n:
-    #         print nnx.nextmove(ip),
-    #         nnx.mutate()
-    #     print
-    
+            
 if __name__ == '__main__':
-    game = runner.Game()
-    
-    # g.push_move(1)
-    # game2input(g)
-    # g.push_move(1)
-    # game2input(g)
-    
-    main(game)
+    main()
