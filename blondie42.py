bdd396f92fb8eff55d2b35d05449a79b4f615cac
kiru <kiru@hulk.(none)>
2013-11-10 19:30:42 -0800

    Turned it into a class

--- blondie42.py
+++ blondie42.py
@@ -5,31 +5,40 @@ import pybrain
 
 from pybrain.structure import FeedForwardNetwork, LinearLayer, SigmoidLayer, FullConnection
 
-n = FeedForwardNetwork()
+class BlondieBrain:
+    def __init__(self,insize=150):
+        self.insize = insize
+        self.nn = FeedForwardNetwork()
 
-inLayer = LinearLayer(150)
-hiddenLayer1 = SigmoidLayer(150)
-hiddenLayer2 = SigmoidLayer(150)
-outLayer = LinearLayer(1)
+        inLayer = LinearLayer(insize)
+        hiddenLayer1 = SigmoidLayer(insize)
+        hiddenLayer2 = SigmoidLayer(insize)
+        outLayer = LinearLayer(1)
 
-n.addInputModule(inLayer)
-n.addModule(hiddenLayer1)
-n.addModule(hiddenLayer2)
-n.addOutputModule(outLayer)
+        self.nn.addInputModule(inLayer)
+        self.nn.addModule(hiddenLayer1)
+        self.nn.addModule(hiddenLayer2)
+        self.nn.addOutputModule(outLayer)
 
-in_to_hidden1 = FullConnection(inLayer, hiddenLayer1)
-hidden1_to_hidden2 = FullConnection(hiddenLayer1, hiddenLayer2)
-hidden2_to_out = FullConnection(hiddenLayer2, outLayer)
+        in_to_hidden1 = FullConnection(inLayer, hiddenLayer1)
+        hidden1_to_hidden2 = FullConnection(hiddenLayer1, hiddenLayer2)
+        hidden2_to_out = FullConnection(hiddenLayer2, outLayer)
 
-n.addConnection(in_to_hidden1)
-n.addConnection(hidden1_to_hidden2)
-n.addConnection(hidden2_to_out)
+        self.nn.addConnection(in_to_hidden1)
+        self.nn.addConnection(hidden1_to_hidden2)
+        self.nn.addConnection(hidden2_to_out)
 
-n.sortModules()
+        self.nn.sortModules()
 
+    def activate(self,inputdata):
+        return self.nn.activate(inputdata)
+
+    def printit(self):
+        print self.nn
+    
 inp = [random.randint(0,6) for _ in range(150)]
 print inp
 
-x = n.activate(inp)
-print n
-print x
+n = BlondieBrain()
+n.activate(inp)
+n.printit()
